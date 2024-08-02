b7175c60e2e82c490c7be8bf8560e91f17ba4bc2
julianthant <julianthant@gmail.com>
2024-08-02 01:30:03 -0700

    chore: Update database configuration for Neon serverless driver

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,8 @@
-// Drizzle example with the Neon serverless driver
+'use server';
+
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
-
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const sql = neon(process.env.DATABASE_URL || '');
 
